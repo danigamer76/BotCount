68d98b4e9ab99bfed7bb1896307524653dda5f03
Daniel Guardia <83751029+danigamer76@users.noreply.github.com>
2024-05-17 16:10:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -220,7 +220,7 @@ async function generarRanking() {
         .sort(([, userDataA], [, userDataB]) => userDataB.totalAbusos - userDataA.totalAbusos)
         .slice(0, 3); // Mostrar solo los primeros 3 puestos
 
-    let message = ":trophy: **Top 3 del ranking** :trophy \n\n";
+    let message = ":trophy: **Top 3 del ranking** :trophy: \n\n";
     sortedRanking.forEach(([username, userData], index) => {
         const { totalAbusos, idSums } = userData;
 
